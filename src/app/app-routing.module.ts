4a33c06992dcb6b56c9dfc93eead0db1d3e8a15d
cgonzalezh333 <claudiogh33@gmail.com>
2021-07-05 18:36:59 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componets/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'LoginVuelosCR',component:LoginComponent},
+  {path:'**',redirectTo:'LoginVuelosCR'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componets/login/login.component';
+
+import { NavbarComponent } from './componets/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
